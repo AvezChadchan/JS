c76110c0abf62573de9a3ed8ef960e262accb4f6
Avez Chadchan <avezchadchan@gmail.com>
2025-07-28 01:05:56 +0530

    .

--- 02_basics/02_basics/01_basics/first.js
+++
@@ -1,7 +0,0 @@
-//  datatypes
-
-//primitive data types (call by value)
-// string number boolean null undefined symbol bigint
-
-// non-primitive data types (call by reference)
-// object array function
